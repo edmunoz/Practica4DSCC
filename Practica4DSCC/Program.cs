97b7c9cedcde8cc3b5fe359b8686965da03dcb3a
unknown <labproto1@cti_thin_53_en.cti.espol.edu.ec>
2015-11-23 16:05:07 -0500

    Lectura de temperatura desde url

--- Practica4DSCC/Program.cs
+++ Practica4DSCC/Program.cs
@@ -25,7 +25,8 @@ namespace Practica4DSCC
         //Objetos de interface gráfica GLIDE
         private GHI.Glide.Display.Window iniciarWindow;
         private Button btn_inicio;
-
+        private TextBlock text;
+        private GT.Timer timer;
         // This method is run when the mainboard is powered up or reset.   
         void ProgramStarted()
         {
@@ -46,6 +47,20 @@ namespace Practica4DSCC
             // Use Debug.Print to show messages in Visual Studio's "Output" window during debugging.
             Debug.Print("Program Started");
 
+            timer = new GT.Timer(3000); // every second (1000ms)
+            timer.Tick += timer_Tick;
+
+            
+
+            //Ethernet
+            this.ethernetJ11D.NetworkInterface.Open();
+            this.ethernetJ11D.NetworkInterface.EnableDhcp();
+            this.ethernetJ11D.UseThisNetworkInterface();
+            this.ethernetJ11D.NetworkDown += ethernetJ11D_NetworkDown;
+            this.ethernetJ11D.NetworkUp += ethernetJ11D_NetworkUp;
+
+  
+
             //Carga la ventana principal
             iniciarWindow = GlideLoader.LoadWindow(Resources.GetString(Resources.StringResources.inicioWindow));
             GlideTouch.Initialize();
@@ -56,11 +71,51 @@ namespace Practica4DSCC
 
             //Selecciona iniciarWindow como la ventana de inicio
             Glide.MainWindow = iniciarWindow;
+
+            text = (TextBlock)Glide.MainWindow.GetChildByName("text_net_status");
+                        
+        }
+
+        void timer_Tick(GT.Timer timer)
+        {
+            HttpRequest request = HttpHelper.CreateHttpGetRequest("http://api.thingspeak.com/channels/46434/fields/2/last");
+            request.ResponseReceived += request_ResponseReceived;  
+            request.SendRequest();
+            Debug.Print("SendRequest");
+        }
+
+        void request_ResponseReceived(HttpRequest sender, HttpResponse response)
+        {
+            var x = response.Text;
+            Debug.Print(x);
+        }
+
+        void ethernetJ11D_NetworkUp(GTM.Module.NetworkModule sender, GTM.Module.NetworkModule.NetworkState state)
+        {
+            timer.Start();
+            this.btn_inicio.Enabled = true;
+            text.Text = ethernetJ11D.NetworkSettings.IPAddress;
+            Glide.MainWindow = iniciarWindow;
+            Debug.Print("Activado");
+        }
+
+        void ethernetJ11D_NetworkDown(GTM.Module.NetworkModule sender, GTM.Module.NetworkModule.NetworkState state)
+        {
+            timer.Stop();
+            this.btn_inicio.Enabled = false;
+            text.Text = "No Network";
+            Glide.MainWindow = iniciarWindow;
+            Debug.Print("Desactivado");
         }
 
         void btn_inicio_TapEvent(object sender)
         {
-            Debug.Print("Iniciar");
+            
+            
+            Debug.Print("Iniciar");      
+
+
+
         }
     }
 }
